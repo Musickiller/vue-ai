ac9e226a59a16a584e056973d59f88e5e076b83d
d.bobyrev <d.bobyrev@astrovolga.ru>
2024-05-21 18:46:32 +0300

    migrate component to new proj

---
+++ src/components/InputField.vue
@@ -0,0 +1,17 @@
+<template>
+  <label for="ai_prompt">
+    <input
+      id="ai_prompt"
+      v-model="modelValue"
+      type="text"
+      v-bind="$attrs"
+      p="x-4 y-2"
+      w="250px"
+      text="center"
+      bg="transparent"
+      border="~ rounded gray-200 dark:gray-700"
+      outline="none active:none"
+    />
+    Prompt for AI
+  </label>
+</template>
